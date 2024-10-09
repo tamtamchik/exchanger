f993ae4fb490693f7b62143f42c57569da4936e7
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-10-09 16:21:21 +0100

    chore: migrate jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
   coverageReporters: ["text", "lcov"],
 };
+
+export default config;
